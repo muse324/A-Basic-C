77cfda41033310ba4fe544b98ab69cac67e936ad
muse324 <hashida@m-use.net>
2022-12-05 22:04:43 +0900

    int変数の足し算をPythonと比較

--- 221206_関数呼び出し/00_addfunc.c
+++ 221206_関数呼び出し/00_addfunc.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 // 数字を出力する
+
+//  # Pythonの場合
+//   sum = 1
+//   print(sum)
 int main()
 {
-    int x = 0;
-    printf("i = %d", x);
+    int x = 1;
+    int sum = 0;
+
+    while (x < 5)
+    {
+        printf("x:%d\n", x);
+        sum += x++;
+    }
+    printf("sum = %d\n", sum);
 }
---
+++ 221206_関数呼び出し/00_addfunc.py
@@ -0,0 +1,8 @@
+# Pythonの場合
+x = 1
+sum = 0
+while x < 5:
+    print(x)
+    sum += x
+    x += 1
+print(sum)
