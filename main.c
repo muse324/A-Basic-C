81f16ba5e960e9ef0b3cfe97a1f746bbdfc0225f
muse324 <hashida@m-use.net>
2022-12-07 03:35:33 +0900

    runcode 実行設定

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-void call_function(void);
-int main(void)
-{
-    call_function();
 
-    return 0;
-}
-void call_function(void)
+int main(void)
 {
     printf("C言語は習うより慣れよ\n");
 }
